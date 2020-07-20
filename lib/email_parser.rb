c399ef86e3ca2e207017d8bbd82ac215c08a9982
Micah Bowie <micahbowie20@gmail.com>
2020-07-20 23:52:02 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,38 @@
+class EmailAddressParser
+  #attr_accessor :email_addresses
+  @list = []
+  
+  def initialize(emails)
+    email_addresses = emails
+    @list << email_addresses
+    #@list << email_addresses
+  end 
+    
+  def parse
+   # @list.join(",")
+  end 
+
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Build a class EmailAddressParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
